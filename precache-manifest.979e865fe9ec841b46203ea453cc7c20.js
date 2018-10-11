3c0d5dee6ed491db80d07ae3106b767f2953f223
Eduardo Hernández <eduardo.h@cinemasapp.co>
2018-10-10 19:40:45 -0500

    Updates

---
+++ precache-manifest.979e865fe9ec841b46203ea453cc7c20.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "49b9231f802555ede5682cfa8b42f7ef",
+    "url": "/grin/static/media/02.49b9231f.png"
+  },
+  {
+    "revision": "441569470a9db16a8df1",
+    "url": "/grin/static/css/main.d16843ae.chunk.css"
+  },
+  {
+    "revision": "a16f00917c74b5e6af58",
+    "url": "/grin/static/js/1.a16f0091.chunk.js"
+  },
+  {
+    "revision": "27bc56d1804d0976f8c2",
+    "url": "/grin/static/js/runtime~main.27bc56d1.js"
+  },
+  {
+    "revision": "1798859a2b3306f3870ad3e73c232e70",
+    "url": "/grin/static/media/tw-icon.1798859a.png"
+  },
+  {
+    "revision": "04114f5c76d5c39538e08bd3c85ad3f2",
+    "url": "/grin/static/media/in-icon.04114f5c.jpg"
+  },
+  {
+    "revision": "a4d589e3a0eddf1b310175dc5aaeea3c",
+    "url": "/grin/static/media/grin.a4d589e3.png"
+  },
+  {
+    "revision": "23b2a55e4d01d12696ac7fd5b00a0389",
+    "url": "/grin/static/media/somos_grin.23b2a55e.jpg"
+  },
+  {
+    "revision": "300f5fc620cc247573da02ab304b6c25",
+    "url": "/grin/static/media/01.300f5fc6.png"
+  },
+  {
+    "revision": "441569470a9db16a8df1",
+    "url": "/grin/static/js/main.44156947.chunk.js"
+  },
+  {
+    "revision": "420da8c3ffa79bd62ae4d10718772cbe",
+    "url": "/grin/static/media/03.420da8c3.png"
+  },
+  {
+    "revision": "8aee9cd8eed5d1321583565f4fe865a7",
+    "url": "/grin/static/media/Dj2N06dUwAAQZuf.8aee9cd8.jpg"
+  },
+  {
+    "revision": "b01e08af885f23d181ad6728483ea591",
+    "url": "/grin/static/media/DjIf915U8AAuwSC.b01e08af.jpg"
+  },
+  {
+    "revision": "0648459991510717821092ecc32c697f",
+    "url": "/grin/static/media/DjXwDbFU4AA4jv_.06484599.jpg"
+  },
+  {
+    "revision": "5be857cb74abac9208e586df6d70b662",
+    "url": "/grin/static/media/DnjN-k8UwAEwGDw.5be857cb.jpg"
+  },
+  {
+    "revision": "d3ea84c4aa9cef0d9f08377da3ad489b",
+    "url": "/grin/static/media/nuckle_semibold_web.d3ea84c4.ttf"
+  },
+  {
+    "revision": "9806e4e1fed59b2e3cc3b9a24713588c",
+    "url": "/grin/static/media/nuckle_bold_web.9806e4e1.ttf"
+  },
+  {
+    "revision": "70294feeebb6ae7b26cc5e6a69b80362",
+    "url": "/grin/static/media/nuckle_book_web.70294fee.ttf"
+  },
+  {
+    "revision": "72c52859e2bc5135fd14f9bc78759f4c",
+    "url": "/grin/index.html"
+  }
+];
